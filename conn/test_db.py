ee61fe0ad24b642a6c4ffdc1196d4fe4b4fc111c
Flávio Micheletti <flaviomicheletti@hotmail.com>
2023-06-12 17:53:22 -0300

    conn

---
+++ conn/test_db.py
@@ -0,0 +1,25 @@
+# test_my_database.py
+import unittest
+from unittest.mock import patch
+from peewee import Model, CharField
+from conn import db
+
+
+# Mock model
+class MockModel(Model):
+    name = CharField()
+
+    class Meta:
+        database = db
+
+class TestMyDatabase(unittest.TestCase):
+
+    @patch('conn.SqliteDatabase')
+    def test_database_creation(self, mock_sqlite_db):
+        # Set up the mock
+        mock_instance = mock_sqlite_db.return_value
+        mock_instance.connect.return_value = None  # Mock the connect() method
+
+        # Call the code that creates the database
+        db.create_tables([MockModel])
+
